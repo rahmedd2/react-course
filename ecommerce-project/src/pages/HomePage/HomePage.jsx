2158f6d2f19c67003d21c45420f0a419786bb5f6
ridwan <rahmed10@uoguelph.ca>
2025-12-24 15:27:11 -0500

    use backend to recieve products data and generate html rather than having data kept in a file

--- ecommerce-project/src/pages/HomePage/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,29 @@
 import "./HomePage.css"
 import { Header } from "../../components/Header"
-import { products } from "../../data/products.js"
+import { useEffect, useState } from "react"
 import { Product } from "../../components/Product.jsx"
+import axios from "axios"
 
 export function HomePage() {
+  // one way to fetch data from backend 
+  // fetch(  //sends a request to the backend with the specified url 
+  //   "http://localhost:3000/api/products"
+  // )
+  //   .then((response)=>{ //use then to wait for the response
+  //     return response.json() //gives us the actual data 
+  //   }).then((data)=>{
+  //       console.log(data) // print the data in the console
+  //   });
+
+  const [products, setProducts] = useState([]);
+  //using axios to fetch data
+  useEffect(()=>{
+    axios.get("http://localhost:3000/api/products") //response that is sent back gives us the data we want using axios
+    .then((response)=>{
+       setProducts(response.data);
+    })
+  },[]);
+
   return (
     <>
       <title>Home</title>
